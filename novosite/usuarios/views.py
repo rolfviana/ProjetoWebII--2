4a723c903976a020735d4c7e95a79f37cdebe70c
rolfviana <rodolfovia1@hotmail.com>
2026-05-21 21:59:53 -0400

    v 1.00 commit depois

--- novosite/usuarios/views.py
+++ novosite/usuarios/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
 
 def login(request):
-    return render(request, 'usuarios/login.html')
+    if request.method == 'GET':
+        return render(request, 'usuarios/login.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        user = authenticate(request, username=email, password=senha)
+        if user is not None:
+            login_django(request, user)
+            return HttpResponse('Login autenticado com sucesso.')
+        else:
+            return HttpResponse('e-mail ou senha inválidos.')
